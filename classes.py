8f4c7bd21e52d0bf9ab62af4b4a0db99f3fc99e7
NM00200 <nickwd516@gmail.com>
2024-07-08 17:52:40 +0100

    log loss is not working

--- classes.py
+++ classes.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn 
 from torch.functional import F
 
+import numpy as np
+
 class MLP(nn.Module):
 
     def __init__(self, decomp_dim: int):
@@ -11,21 +13,53 @@ class MLP(nn.Module):
         self.fc2 = nn.Linear(64, 64)
         self.fc3 = nn.Linear(64, decomp_dim)
 
-        self._log_sigma_base = torch.log(torch.FloatTensor([1]))
-        # self._log_sigma = self._log_sigma_base
-        self._log_sigma = nn.Parameter(data=self._log_sigma_base+torch.log(torch.rand(1)))
+        self._log_sigma_base = torch.log(torch.FloatTensor([0.1]))
+        self._log_sigma = self._log_sigma_base
+        # self._log_sigma = nn.Parameter(data=self._log_sigma_base+torch.log(torch.rand(1)))
 
-        self.price_impact_kappa_base = torch.FloatTensor([1])
-        # self.price_impact_kappa = self.price_impact_kappa_base
-        self.price_impact_kappa = nn.Parameter(data=self.price_impact_kappa_base*torch.rand(1))
+        self.price_impact_kappa_base = torch.FloatTensor([0.01])
+        self.price_impact_kappa = self.price_impact_kappa_base
+        # self.price_impact_kappa = nn.Parameter(data=self.price_impact_kappa_base*torch.rand(1))
 
     def forward(self, x):
 
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x_one= F.relu(self.fc1(x))
+        x_two = F.relu(self.fc2(x_one)) 
+        x_three = self.fc3(x_two)
 
-        return x
+        return x_three
+    
+    def approx_func(self, s, t):
+
+        s_val = self.forward(s)
+        t_val = self.forward(t)
+
+        return (s_val * t_val).sum(axis=1)
+    
+    def numpy_approx_func(self, s, t):
+        
+        if type(s) == float:
+            s_in_val = np.array([s])
+            t_in_val = np.array([t])
+        else:
+            s_in_val = s
+            t_in_val = t
+
+        s = torch.FloatTensor(s_in_val).reshape(-1,1)
+        t = torch.FloatTensor(t_in_val).reshape(-1,1)
+
+        s_val = self.forward(s)
+        t_val = self.forward(t)
+
+        return (s_val * t_val).sum(axis=-1).detach().numpy()
+    
+    def permenant_price_impact_func(self, nu):
+
+        return self.kappa * nu
+    
+    def numpy_permenant_price_impact_func(self, nu):
+
+        return self.kappa.detach().item() * nu
 
     @property
     def sigma(self):
@@ -40,14 +74,14 @@ class MultiTaskLoss(nn.Module):
     def __init__(self, num_losses: int):
         super(MultiTaskLoss, self).__init__()
 
-        self._log_params = nn.Parameter(data=torch.ones(num_losses))
+        self._log_params = nn.Parameter(data=torch.ones(num_losses), requires_grad=True)
 
         return None
     
     def forward(self, loss):
 
-        stds = torch.exp(self._log_params)
+        stds = torch.exp(self._log_params)**0.5
 
-        total_loss = (1/(1 + stds)) * loss + torch.log(stds)
+        total_loss = (1/(1 + stds)) * loss + torch.log(stds)**2
 
         return total_loss.mean()
--- utils.py
+++ utils.py
@@ -9,25 +9,62 @@ def non_markovian_midprice(inital_prices,
                         num_paths,
                         num_timesteps,
                         sigma):
-
-    final_time = num_timesteps*dt
-
-    permenant_price_impact_vals = permenant_price_impact_func(nu_vals)
-
-    # This gives K(s, t) for s<=t to integrate over
-    kernel_vals = np.array([kernel_function(timestep*dt, final_time) for timestep in range(num_timesteps)])
-    kernel_integrand = kernel_vals.reshape(-1, 1) * permenant_price_impact_vals * dt
-
-    # We do a vstack as the axis=1 are the batches
-    kernel_integrand = np.vstack([inital_prices, kernel_integrand])
-    kernel_integral = np.cumsum(kernel_integrand, axis=0)
-
+    
     noise_process = rng.normal(0,1, size=(num_timesteps, num_paths)) 
     brownian_motion_integrand = sigma*(dt**0.5)*noise_process
     zeros = np.zeros((1, num_paths))
     brownian_motion_integrand = np.vstack([zeros, brownian_motion_integrand])
-
     brownian_motion_integral = np.cumsum(brownian_motion_integrand, axis=0)
-    prices = kernel_integral+brownian_motion_integral
 
+    prices = [inital_prices]
+    for super_timestep in range(0, num_timesteps):
+        final_time = super_timestep*dt
+
+        permenant_price_impact_vals = permenant_price_impact_func(nu_vals[:super_timestep])
+        # This gives K(s, t) for s<=t to integrate over
+        kernel_vals = np.array([kernel_function(timestep*dt, final_time) for timestep in range(super_timestep)]) 
+        kernel_integrand = kernel_vals.reshape(-1, 1) * permenant_price_impact_vals * dt
+
+        # We do a vstack as the axis=1 are the batches
+        kernel_integrand = np.vstack([inital_prices, kernel_integrand])
+        kernel_integral = np.sum(kernel_integrand, axis=0)
+
+        s_t = kernel_integral + brownian_motion_integral[super_timestep]
+
+        prices.append(s_t)
+    
+    prices = np.vstack(prices)
+    
     return prices, noise_process
+
+# def non_markovian_midprice(inital_prices, 
+#                         permenant_price_impact_func, 
+#                         nu_vals,
+#                         kernel_function, 
+#                         dt, 
+#                         rng,
+#                         num_paths,
+#                         num_timesteps,
+#                         sigma):
+
+#     final_time = num_timesteps*dt
+
+#     permenant_price_impact_vals = permenant_price_impact_func(nu_vals)
+
+#     # This gives K(s, t) for s<=t to integrate over
+#     kernel_vals = np.array([kernel_function(timestep*dt, final_time) for timestep in range(num_timesteps)])
+#     kernel_integrand = kernel_vals.reshape(-1, 1) * permenant_price_impact_vals * dt
+
+#     # We do a vstack as the axis=1 are the batches
+#     kernel_integrand = np.vstack([inital_prices, kernel_integrand])
+#     kernel_integral = np.cumsum(kernel_integrand, axis=0)
+
+#     noise_process = rng.normal(0,1, size=(num_timesteps, num_paths)) 
+#     brownian_motion_integrand = sigma*(dt**0.5)*noise_process
+#     zeros = np.zeros((1, num_paths))
+#     brownian_motion_integrand = np.vstack([zeros, brownian_motion_integrand])
+
+#     brownian_motion_integral = np.cumsum(brownian_motion_integrand, axis=0)
+#     prices = kernel_integral+brownian_motion_integral
+
+#     return prices, noise_process
