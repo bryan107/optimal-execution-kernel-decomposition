f1839292c2f52ad9a38f4b6307ebfb6cc94e6025
NM00200 <nickwd516@gmail.com>
2024-07-17 21:42:07 +0100

    some corrections

--- utils.py
+++ utils.py
@@ -37,6 +37,45 @@ def non_markovian_midprice(inital_prices,
     
     return prices, noise_process
 
+def vol_non_markovian_midprice(inital_prices, 
+                           permenant_price_impact_func, 
+                           nu_vals,
+                           drift_kernel_function, 
+                           vol_kernel_function,
+                           dt, 
+                           rng,
+                           num_paths,
+                           num_timesteps,
+                           sigma):
+    
+    noise_process = rng.normal(0,1, size=(num_timesteps, num_paths))
+
+    prices = []
+    for super_timestep in range(0, num_timesteps):
+        final_time = super_timestep*dt
+
+        permenant_price_impact_vals = permenant_price_impact_func(nu_vals[:super_timestep])
+
+        # This gives K1(s, t) for s<=t to integrate over
+        kernel_vals = np.array([drift_kernel_function(timestep*dt, final_time) for timestep in range(super_timestep)]) 
+        kernel_integrand = kernel_vals.reshape(-1, 1) * permenant_price_impact_vals * dt
+        # We do a vstack as the axis=1 are the batches
+        kernel_integrand = np.vstack([inital_prices, kernel_integrand])
+        kernel_integral = np.sum(kernel_integrand, axis=0)
+
+        # This gives K2(s, t) for s<=t to integrate over
+        kernel_vals_vol = np.array([vol_kernel_function(timestep*dt, final_time) for timestep in range(super_timestep)]).reshape(-1, 1)
+        kernel_integrand_vol = sigma*kernel_vals_vol*(dt**0.5)*noise_process[:super_timestep, :]
+        kernel_integral_vol = np.sum(kernel_integrand_vol, axis=0)
+        
+        s_t = kernel_integral + kernel_integral_vol
+
+        prices.append(s_t)
+    
+    prices = np.vstack(prices)
+    
+    return prices, noise_process
+
 # def non_markovian_midprice(inital_prices, 
 #                         permenant_price_impact_func, 
 #                         nu_vals,
